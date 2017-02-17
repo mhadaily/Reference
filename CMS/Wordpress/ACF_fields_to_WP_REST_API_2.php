0438740d121e9e3fb8d760be2db862a726c11321
Majid <mhadaily@gmail.com>
2017-02-17 17:53:31 +0800

    Add ACF to REST api response in Wordpress

---
+++ CMS/Wordpress/ACF_fields_to_WP_REST_API_2.php
@@ -0,0 +1,22 @@
+<?php
+// This is an example that how you are able to customize WP REST API and and ACF to get_callback
+//In this example STEP is your custom post type and can be any other post type in wordpress such as post , page and ...
+
+add_action( 'rest_api_init', 'slug_register_acf_random' );
+function slug_register_acf_random() {
+    register_rest_field( 'step',
+        'acf',
+        array(
+            'get_callback'    => 'slug_get_acf',
+            'update_callback' => '',
+            'schema'          => null,
+        )
+    );
+}
+
+function slug_get_acf( $object, $field_name, $request ) {
+    if (function_exists('get_fields')) {
+        return get_fields($data['id']);
+    }
+    return [];
+}
