47292a0608117adabf1bb789e993d017c3f79fc1
Majid <mhadaily@gmail.com>
2017-06-13 19:23:58 +0200

    formating

--- EmberJS/modify-ajax-request.js
+++ EmberJS/modify-ajax-request.js
@@ -1,14 +1,14 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
-export default DS.RESTAdapter.extend( {
-
-    // always send Ember=1 in params
-    ajax(url, type, hash) {
-        if (Ember.isEmpty(hash)) hash = {};
-        if (Ember.isEmpty(hash.data)) hash.data = {};
-        hash.data.ember = 1;
-        return this._super(url, type, hash);
-    },
-
+export default DS.RESTAdapter.extend({
+  
+  // always send Ember=1 in params
+  ajax(url, type, hash) {
+    if (Ember.isEmpty(hash)) hash = {};
+    if (Ember.isEmpty(hash.data)) hash.data = {};
+    hash.data.ember = 1;
+    return this._super(url, type, hash);
+  },
+  
 });
